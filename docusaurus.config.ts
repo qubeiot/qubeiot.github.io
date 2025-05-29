528fd3826910c5c2fbfdc8f8869fb19f231eca79
bradr <bradroger@gmail.com>
2025-05-29 10:39:52 -0700

    warn on broken links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'qubeiot', // Usually your GitHub org/user name.
   projectName: 'qubedocs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
